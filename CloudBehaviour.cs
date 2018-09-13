2e7f037977b93a582b025c5e033cad04801db3cc
Joar <42805245+Shrimpu@users.noreply.github.com>
2018-09-13 10:33:38 +0200

    Cloud Script
    Please read the text for help on setting it up.

---
+++ CloudBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaviour : MonoBehaviour
+{
+    public float speed;
+
+	void Start ()
+    {
+        speed = Random.Range(0.005f, 0.05f);
+        transform.position = new Vector3(transform.position.x, transform.position.y - (speed*40));
+	}
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x - speed,transform.position.y);
+    }
+}
---
+++ CloudSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public float spawnTime;
+    public GameObject cloud;
+
+	void Update ()
+    {
+        if (spawnTime >= 0)
+        {
+            spawnTime -= Time.deltaTime;
+        }
+        else if (spawnTime <= 0)
+        {
+            Instantiate(cloud);
+            TimeReset();
+        }
+	}
+
+    public void TimeReset()
+    {
+        spawnTime = Random.Range(1f, 5f);
+    }
+}
